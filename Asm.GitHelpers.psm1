d9abf422ebad4d608f7fdd4ad0adba1752c69abc
Andrew McLachlan <andy@mclachlan.family>
2025-02-26 19:24:25 +1000

    Added ability to purge branches where the tracking branch has gone.

--- Asm.GitHelpers.psm1
+++ Asm.GitHelpers.psm1
@@ -1,10 +1,39 @@
+<#
+    .SYNOPSIS
+        Removes all merged Git branches from current repository.
+#>
 Function Remove-GitBranches
 {
     [CmdletBinding()]
+    param(
+        [Alias("g")]
+        [Parameter(Mandatory = $false)]
+        [switch]$Gone,
+        [Parameter(Mandatory = $false)]
+        [switch]$Force,
+        [Parameter(Mandatory = $false)]
+        [switch]$WhatIf
+    )
 
-    $branches = git branch --merged | Select-String -Pattern "(^\*|main)" -NotMatch
+    if ($Gone)
+    {
+        $branches = git branch -vv | Where-Object{$_ -match ": gone"} | ForEach-Object{($_ -split "\s+")[1]} 
+    }
+    else
+    {
+        $branches = git branch --merged | Select-String -Pattern "(^\*|main|master)" -NotMatch
+    }
 
-    $branches | ForEach-Object | git branch -d $_.Line
+    $switch = $Force ? "-D" : "-d"
+
+    foreach ($branch in $branches)
+    {
+        $branchName = $branch.Line.Trim()
+        Write-Host "Deleting branch: $branchName"
+        if ($WhatIf) { continue }
+        git branch $switch $branchName
+    }
+    #$branches | ForEach-Object | git branch -d $_.Line
 }
 
-Export-ModuleMember Remove-GitBranches
+Export-ModuleMember -Cmdlet Remove-GitBranches
